35b7b43bc4c9033583113462fed5639490248ea0
Samuel Custer <samuel.a.custer@gmail.com>
2018-11-11 01:42:34 -0500

    Grammar Fix

--- say.lua
+++ say.lua
@@ -1,3 +1,3 @@
 options = {
-    FullInventoryNotifierSayMsg = "My inventory is full, please give me moment to destroy some items to make room."
+    FullInventoryNotifierSayMsg = "My inventory is full, please give me a moment to destroy some items to make room."
 };
